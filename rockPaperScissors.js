e9e5eeec071b02e93bfb833c1e038a591c3669c8
therealwumbologist <gallo7337@gmail.com>
2021-11-18 18:20:05 -0500

    Added play function: contains logic to play a single game against the CPU using a hard-coded playerChoice, and return the result of the game.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,5 @@
+let playerChoice = "sCissors";
+
 function computerPlay() {
     let selection = Math.floor(Math.random()*3) + 1;
     if (selection === 1) {
@@ -9,4 +11,34 @@ function computerPlay() {
     }
 }
 
-console.log(computerPlay());
+function play(playerSelection, computerSelection) {
+    let playerSelectInsens = playerSelection.toLowerCase();
+
+    if (playerSelectInsens === "rock") {
+        if (computerSelection === "rock") {
+            return `It's a tie! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        } else if (computerSelection === "paper") {
+            return `You lose! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        } else if (computerSelection === "scissors") {
+            return `You win! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        }
+    } else if (playerSelectInsens === "paper") {
+        if (computerSelection === "rock") {
+            return `You win! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        } else if (computerSelection === "paper") {
+            return `It's a tie! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        } else if (computerSelection === "scissors") {
+            return `You lose! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        }
+    } else if (playerSelectInsens === "scissors") {
+        if (computerSelection === "rock") {
+            return `You lose! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        } else if (computerSelection === "paper") {
+            return `You win! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        } else if (computerSelection === "scissors") {
+            return `It's a tie! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+        }
+    }
+}
+
+console.log(play(playerChoice, computerPlay()));

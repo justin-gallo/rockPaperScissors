08b2aef09453d9580bb89293c2ee7885bf437464
therealwumbologist <gallo7337@gmail.com>
2021-11-21 14:59:29 -0500

    Added body content to index.html, and created styles.css. Modified rockPaperScissors.js to listen for button clicks, and execute playRound() function for the respective playerSelection.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,77 +9,84 @@ function computerPlay() {
     }
 }
 
-function play(computerSelection) { // Plays a single game.
-    
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
-    let playerSelectInsens = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection) { // Plays a single game.
 
-    if (playerSelectInsens === "rock") {
+    if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            return `It's a tie! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `It's a tie! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         } else if (computerSelection === "paper") {
-            return `You lose! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `You lose! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         } else if (computerSelection === "scissors") {
-            return `You win! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `You win! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         }
-    } else if (playerSelectInsens === "paper") {
+    } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            return `You win! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `You win! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         } else if (computerSelection === "paper") {
-            return `It's a tie! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `It's a tie! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         } else if (computerSelection === "scissors") {
-            return `You lose! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `You lose! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         }
-    } else if (playerSelectInsens === "scissors") {
+    } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            return `You lose! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `You lose! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         } else if (computerSelection === "paper") {
-            return `You win! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `You win! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         } else if (computerSelection === "scissors") {
-            return `It's a tie! You chose: ${playerSelectInsens}. The computer chose: ${computerSelection}.`;
+            return `It's a tie! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
         }
     }
 }
 
-function game() {
-    let playerWinCount = 0;
-    let playerLoseCount = 0;
-    let tieCount = 0;
+const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', () => {
+    console.log(playRound('rock', computerPlay()));
+});
 
-    for (let i = 1; i <= 5; i++) { //Run the play function five times and record results.
-        let result = play(computerPlay());
-        console.log(result);
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', () => {
+    console.log(playRound('paper', computerPlay()));
+});
+
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click', () => {
+    console.log(playRound('scissors', computerPlay()));
+});
+
+// function game() {
+//     let playerWinCount = 0;
+//     let playerLoseCount = 0;
+//     let tieCount = 0;
+
+//     for (let i = 1; i <= 5; i++) { //Run the play function five times and record results.
+//         let result = playRound(computerPlay());
+//         console.log(result);
         
-        if (result.includes("win")) {
-            playerWinCount = ++playerWinCount;
-        } else if (result.includes("lose")) {
-            playerLoseCount = ++playerLoseCount;
-        } else if (result.includes('tie')) {
-            tieCount = ++tieCount;
-        }
-    }
+//         if (result.includes("win")) {
+//             playerWinCount = ++playerWinCount;
+//         } else if (result.includes("lose")) {
+//             playerLoseCount = ++playerLoseCount;
+//         } else if (result.includes('tie')) {
+//             tieCount = ++tieCount;
+//         }
+//     }
 
-    if (playerWinCount > playerLoseCount) {
-        return `You win the match! Final score is: 
-        Player: ${playerWinCount}
-        Computer: ${playerLoseCount}
-        Ties: ${tieCount}`
-    } else if (playerWinCount < playerLoseCount) {
-        return `You lose the match! Final score is: 
-        Player: ${playerWinCount}
-        Computer: ${playerLoseCount}
-        Ties: ${tieCount}`
-    } else if (playerWinCount === playerLoseCount) {
-        return `The match is a tie! Final score is: 
-        Player: ${playerWinCount}
-        Computer: ${playerLoseCount}
-        Ties: ${tieCount}`
-    }
+//     if (playerWinCount > playerLoseCount) {
+//         return `You win the match! Final score is: 
+//         Player: ${playerWinCount}
+//         Computer: ${playerLoseCount}
+//         Ties: ${tieCount}`
+//     } else if (playerWinCount < playerLoseCount) {
+//         return `You lose the match! Final score is: 
+//         Player: ${playerWinCount}
+//         Computer: ${playerLoseCount}
+//         Ties: ${tieCount}`
+//     } else if (playerWinCount === playerLoseCount) {
+//         return `The match is a tie! Final score is: 
+//         Player: ${playerWinCount}
+//         Computer: ${playerLoseCount}
+//         Ties: ${tieCount}`
+//     }
+// }
 
-    // To-do: 
-    // - Rework play() function to return a global variable.
-    // - Increment the value of playerWinCount, playerLoseCount, tieCount after each game. 
-    // - Compare counter variables, and return winner and loser of match with results. 
-}
-
-console.log(game());
+// console.log(game());

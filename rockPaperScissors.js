458fc1302ce5c10d9e906af04f98b286c0d169b1
therealwumbologist <gallo7337@gmail.com>
2021-12-03 16:34:59 -0500

    Completed UI and logic to play a first to 5 match. Added reset button on win/loss that resets the game.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,7 @@
+let winCount = 0;
+let loseCount = 0;
+let tieCount = 0;
+
 function computerPlay() {
     let selection = Math.floor(Math.random()*3) + 1;
     if (selection === 1) {
@@ -10,77 +14,158 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) { // Plays a single game.
-
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `It's a tie! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        } else if (computerSelection === "paper") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `You lose! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        } else if (computerSelection === "scissors") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `You win! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
+    if (winCount >= 5 || loseCount >= 5) {
+        return;
+    } else {
+        if (playerSelection === "rock") {
+            if (computerSelection === "rock") {
+                tieResult();
+            } else if (computerSelection === "paper") {
+                loseCountAppend();
+            } else if (computerSelection === "scissors") {
+                winCountAppend();
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                winCountAppend();
+            } else if (computerSelection === "paper") {
+                tieResult();
+            } else if (computerSelection === "scissors") {
+                loseCountAppend();
+            }
+        } else if (playerSelection === "scissors") {
+            if (computerSelection === "rock") {
+                loseCountAppend();
+            } else if (computerSelection === "paper") {
+                winCountAppend();
+            } else if (computerSelection === "scissors") {
+                tieResult();
+            }
         }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `You win! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        } else if (computerSelection === "paper") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `It's a tie! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        } else if (computerSelection === "scissors") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `You lose! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `You lose! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        } else if (computerSelection === "paper") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `You win! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        } else if (computerSelection === "scissors") {
-            const result = document.createElement('div');
-            result.classList.add('result');
-            result.textContent = `It's a tie! You chose: ${playerSelection}. The computer chose: ${computerSelection}.`;
-            resultContainer.appendChild(result);
-        }
-    }
+    } 
 }
 
 const rockBtn = document.querySelector('.rock');
 rockBtn.addEventListener('click', () => {
-    console.log(playRound('rock', computerPlay()));
+    playRound('rock', computerPlay());
 });
 
 const paperBtn = document.querySelector('.paper');
 paperBtn.addEventListener('click', () => {
-    console.log(playRound('paper', computerPlay()));
+    playRound('paper', computerPlay());
 });
 
 const scissorsBtn = document.querySelector('.scissors');
 scissorsBtn.addEventListener('click', () => {
-    console.log(playRound('scissors', computerPlay()));
+    playRound('scissors', computerPlay());
 });
 
-const resultContainer = document.querySelector('.resultContainer');
+function winCountAppend() {
+    const targetWin = document.querySelector('.winCount');
+    const targetLose = document.querySelector('.loseCount');
+    let feedback = document.querySelector('.feedback');
+    winCount++;
+    targetWin.textContent = `${winCount}`;
+    feedback.textContent = `You won this one! You've got ${winCount} wins out of 5. Keep going!`;
+    targetWin.classList.add('glowCount');
+    setTimeout(function(){
+        targetWin.classList.remove('glowCount');
+    }, 500);
+
+    if (winCount === 5) {
+        let feedback = document.querySelector('.feedback');
+        feedback.textContent = `You win! Click the reset button to play again.`;
+        const container = document.querySelector('#main');
+        const reset = document.createElement('button');
+        reset.classList.add('reset');
+        reset.classList.add('resetHover');
+        reset.textContent = `RESET GAME`;
+        container.appendChild(reset);
+        const resetBtn = document.querySelector('.reset');
+        resetBtn.addEventListener('click', () => {
+            winCount = 0;
+            targetWin.textContent = `${winCount}`;
+            loseCount = 0;
+            targetLose.textContent = `${loseCount}`;
+            container.removeChild(reset);
+        });
+    }
+
+    if (loseCount === 5) {
+        let feedback = document.querySelector('.feedback');
+        feedback.textContent = `You lost! Click the reset button to play again.`;
+        const container = document.querySelector('#main');
+        const reset = document.createElement('button');
+        reset.classList.add('reset');
+        reset.classList.add('resetHover');
+        reset.textContent = `RESET GAME`;
+        container.appendChild(reset);
+        const resetBtn = document.querySelector('.reset');
+        resetBtn.addEventListener('click', () => {
+            winCount = 0;
+            targetWin.textContent = `${winCount}`;
+            loseCount = 0;
+            targetLose.textContent = `${loseCount}`;
+            container.removeChild(reset);
+        });
+    }
+}
+
+function loseCountAppend() {
+    const targetWin = document.querySelector('.winCount');
+    const targetLose = document.querySelector('.loseCount');
+    let feedback = document.querySelector('.feedback');
+    loseCount++;
+    targetLose.textContent = `${loseCount}`;
+    feedback.textContent = `You lost that one. The CPU has ${loseCount} wins out of 5. Don't give up!`;
+    targetLose.classList.add('glowCount');
+    setTimeout(function(){
+        targetLose.classList.remove('glowCount');
+    }, 500);
+
+    if (winCount === 5) {
+        let feedback = document.querySelector('.feedback');
+        feedback.textContent = `You win! Click the reset button to play again.`;
+        const container = document.querySelector('#main');
+        const reset = document.createElement('button');
+        reset.classList.add('reset');
+        reset.classList.add('resetHover');
+        reset.textContent = `RESET GAME`;
+        container.appendChild(reset);
+        const resetBtn = document.querySelector('.reset');
+        resetBtn.addEventListener('click', () => {
+            winCount = 0;
+            targetWin.textContent = `${winCount}`;
+            loseCount = 0;
+            targetLose.textContent = `${loseCount}`;
+            container.removeChild(reset);
+        });
+    }
+
+    if (loseCount === 5) {
+        let feedback = document.querySelector('.feedback');
+        feedback.textContent = `You lost! Click the reset button to play again.`;
+        const container = document.querySelector('#main');
+        const reset = document.createElement('button');
+        reset.classList.add('reset');
+        reset.classList.add('resetHover');
+        reset.textContent = `RESET GAME`;
+        container.appendChild(reset);
+        const resetBtn = document.querySelector('.reset');
+        resetBtn.addEventListener('click', () => {
+            winCount = 0;
+            targetWin.textContent = `${winCount}`;
+            loseCount = 0;
+            targetLose.textContent = `${loseCount}`;
+            container.removeChild(reset);
+        });
+    }
+}
+
+function tieResult() {
+    let feedback = document.querySelector('.feedback');
+    feedback.textContent = `It's a tie! Try again.`; 
+}
 
 // function game() {
 //     let playerWinCount = 0;
